ac905f099ae509c73b54dd88b5978ead3ffb1fc0
Gaurav <gauravsarma68@gmail.com>
2025-03-22 09:59:11 +0530

    final

--- src/components/Modal/ModalForm.jsx
+++ src/components/Modal/ModalForm.jsx
@@ -76,6 +76,7 @@ const ModalForm = props => {
 
             setMoney({balance: newBalance, expenses: newExpense});
             setTransactionData([...transactionData]);
+
         }
 
         toggleModal();
